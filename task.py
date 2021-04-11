552c1366676f63d119e2a50def4c32743b8e00ab
Andrew Chupakhin <pixtergood@gmail.com>
2021-04-11 18:55:12 +0500

    Update task.py

--- task.py
+++ task.py
@@ -12,6 +12,34 @@ def bubble_sort(nums):
                     swapped = True
 
 
+def inverted_bubble_sort(nums):  # сортировка для области где x, y < 0
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(nums[1]) - 1):
+            if abs(nums[1][i][0]) < abs(nums[1][i + 1][0]):
+                nums[1][i], nums[1][i + 1] = nums[1][i + 1], nums[1][i]
+                swapped = True
+            elif abs((nums[1][i][0])) == abs(nums[1][i + 1][0]):
+                if abs(nums[1][i][1]) > abs(nums[1][i + 1][1]):
+                    nums[1][i], nums[1][i + 1] = nums[1][i + 1], nums[1][i]
+                    swapped = True
+
+
+from math import sqrt
+
+
+def sr_val(nums):
+    summ = 0
+    minn = 999
+    maxx = -999
+    for i in nums:
+        summ += sqrt(i[0]**2 + i[1]**2)
+        minn = min(sqrt(i[0]**2 + i[1]**2), minn)
+        maxx = max(sqrt(i[0]**2 + i[1]**2), maxx)
+    return [summ / len(nums), minn, maxx]
+
+
 from array import array
 
 
@@ -22,7 +50,7 @@ def get_sorted_array():
     points2 = ("i", [])
     points3 = ("i", [])
     points4 = ("i", [])
-    length = 100  # кол-во точек
+    length = 5  # кол-во точек
 
     for i in range(length):
         points[1].append([])
@@ -55,7 +83,7 @@ def get_sorted_array():
             p4 += 1
         bubble_sort(points1)
         bubble_sort(points2)
-        bubble_sort(points3)
+        inverted_bubble_sort(points3)
         bubble_sort(points4)
         output_points = points1[1] + points2[1] + points3[1] + points4[1]
     return output_points
@@ -69,7 +97,8 @@ import re
 def regular_solution(find_for_auto_sign):
     result = []
     for i in find_for_auto_sign:
-        result += re.findall(r'^[ABKMTXOАВЕКМНОРСТУХ]\d{3}[ABKMTXOАВЕКМНОРСТУХ]{2}\d{2,3}', i)
+        result += re.findall(
+            r'^[ABKMTXOАВЕКМНОРСТУХ]\d{3}[ABKMTXOАВЕКМНОРСТУХ]{2}\d{2,3}', i)
     return result
 
 
@@ -97,18 +126,29 @@ def get_of_valid_sign(input_array):
                 output_array.append(input_sign)
     return output_array
 
+
 def main():
     # задание 1
-    print(get_sorted_array(), end = "\n")
+    output = get_sorted_array()
+    print(output)
+    task = sr_val(output)
+    print("задание 1", end="\n")
+    print("среднее значение до точек: ", round(task[0], 3))
+    print("макс значение до точек: ", round(task[2], 3))
+    print("минимальное значение до точек: ", round(task[1], 3))
+    print("задание 2", end="\n")
     #задание 2
     find_for_auto_sign = [
         "AASCB123KK12", "BB1234B1", "А123АА11", "А222АА123", "A12AA123",
         "A123CC1234", "AA123A12", "АА12322"
     ]
     # решение 1
-    print("регулярное выражение: ", regular_solution(find_for_auto_sign), end = "\n")
+    print("регулярное выражение: ",
+          regular_solution(find_for_auto_sign),
+          end="\n")
     # решение 2
-    print("проброс через булево условие: ", get_of_valid_sign(find_for_auto_sign))
+    print("проброс через булево условие: ",
+          get_of_valid_sign(find_for_auto_sign))
 
 
 if __name__ == "__main__":
